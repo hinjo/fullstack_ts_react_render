09fa420d96a0e75d8e091a3d5275f9bdf40d6ebc
Vanja Ferhatovic <vanja.ferhatovic@gmail.com>
2023-08-25 09:23:44 +0200

    backend to ts convert

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,19 @@
-"use strict";
-const cors = require("cors"), express = require("express");
+const cors = require("cors"),
+  express = require("express");
 const path = require("path");
+
 const app = express();
+
 app.use(cors());
+
 app.use(express.static(path.join(path.resolve(), "public")));
+
 app.get("/api", (_request, response) => {
-    response.send({ hello: "World" });
+  response.send({ hello: "World" });
 });
+
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
-    console.log("Redo på http://localhost:8080/");
+  console.log("Redo på http://localhost:8080/");
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch('/api')
+      .then((response) => response.json())
+      .then((result) => {
+        alert(`Hello ${result.hello}!`)
+      })
+  }, [])
 
   return (
     <>
